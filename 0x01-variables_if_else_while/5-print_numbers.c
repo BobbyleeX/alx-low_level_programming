3271e6e38383166f83ab638ca0b9dcd782bad2cc
BobbyleeX <osamoraustine@gmail.com>
2023-02-18 06:37:10 -0800

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,23 +1,21 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 /**
 * main - program will assign a random number to the variable n
-* Return 0 always
+* Return: 0 always
 */
 
 int main(void)
 {
-	char la;
+	int num;
 
-	for (la = 'a'; la <= 'z'; la++)
-	{
-		if (la != 'e' && la != 'q')
-			putchar(la);
-	}
-	putchar('\n');
+	for (num = 0; num < 10; num++)
+		prinf("%d", num);
+
+	printf("\n");
 
 	return (0);
 }
+
